1c28ca110ac0fc7c6463f6edbe53de0a0901d4dd
edgar <edgartomko88@gmail.com>
2023-02-02 07:55:54 +0300

    add registration for clients

--- handlers/other.py
+++ handlers/other.py
@@ -1,8 +1,16 @@
 from aiogram import types, Dispatcher
 from keyboards import get_ikb_menu_other, get_ikb_menu_client
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-clients_list = []
+clients_list = {}
+all_tokens = ['123', '111']
+
+
+class FSMRegistrationUser(StatesGroup):
+    name = State()
+    token = State()
 
 
 async def command_start(message: types.Message):
@@ -14,9 +22,37 @@ async def command_start(message: types.Message):
     else:
         await message.answer(f'Здравствуйте {message.from_user.username}! Your id -> {user_id}',
                              reply_markup=await get_ikb_menu_other())
-
     await message.delete()
+
+
+async def set_new_user(callback: types.CallbackQuery):
+    await FSMRegistrationUser.name.set()
+    await callback.message.answer(f'Введите ваше имя:')
+
+
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+    await FSMRegistrationUser.next()
+    await message.reply('Введите ТОКЕН для регистрации:')
+
+
+async def load_token(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['token'] = message.text
+
+        clients_list[message.from_user.id] = {'name': data['name'], 'token': data['token']}
+        if data['token'] in all_tokens:
+            await message.answer(f'Регистрация завершена, {clients_list}',
+                                 reply_markup=await get_ikb_menu_client())
+        else:
+            await message.answer(f'Вы ввели неверный ТОКЕН, {clients_list}',
+                                 reply_markup=await get_ikb_menu_other())
+    await state.finish()
 
 
 def register_handlers_other(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
+    dp.register_callback_query_handler(set_new_user, text='registration')
+    dp.register_message_handler(load_name, state=FSMRegistrationUser.name)
+    dp.register_message_handler(load_token, state=FSMRegistrationUser.token)
--- init_bot.py
+++ init_bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 
 from data.config import data
 
+storage = MemoryStorage
+
 bot = Bot(token=data.BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage())
